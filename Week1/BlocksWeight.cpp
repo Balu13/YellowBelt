0142d57b200a5b4251348706b8909d6d2f7feb4d
Balu13 <balu13@gmail.com>
2018-10-05 09:42:58 +0300

    Mass of blocks

---
+++ Week1/BlocksWeight.cpp
@@ -0,0 +1,53 @@
+//#include "stdafx.h"
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+struct Block
+{
+	int m_density;
+	int m_w;
+	int m_h;
+	int m_d;
+
+	Block() : m_density(0), m_w(0), m_d(0)
+	{
+	}
+
+	Block(int density, int w, int h, int d) : m_density(density), m_w(w), m_h(h), m_d(d)
+	{
+	}
+
+	uint64_t calcWeight() const
+	{
+		return static_cast<uint64_t>(m_density) * m_w * m_h * m_d;
+	}
+};
+
+int main()
+{
+	int blockNum, density;
+	cin >> blockNum >> density;
+
+	vector<Block> blocks(blockNum);
+	for (int blockNo = 0; blockNo < blockNum; ++blockNo)
+	{
+		int w, h, d;
+		cin >> w >> h >> d;
+		blocks.push_back({ density, w, h, d });
+	}
+
+	uint64_t weightSum{ 0 };
+	for (auto& block : blocks)
+	{
+		weightSum += block.calcWeight();
+	}
+
+	cout << weightSum;
+
+	cin.clear();
+	cin.ignore(32768, '\n');
+	cin.get();
+	return 0;
+}
